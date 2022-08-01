94724bc3e8691de0e63a00cebe750360345b84f6
Shaffan <shaffan1996@gmail.com>
2022-08-01 16:52:44 -0400

    create read update delete functionality works. working on searchung functionality

---
+++ Sharpreads/Data/Migrations/20220801170357_InitialSetup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sharpreads.Data.Migrations
+{
+    public partial class InitialSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    Summary = table.Column<string>(nullable: true),
+                    Rating = table.Column<double>(nullable: true),
+                    Created = table.Column<DateTime>(nullable: true),
+                    CoverURL = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
---
+++ Sharpreads/Models/Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sharpreads.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Summary { get; set; }
+        public double? Rating { get; set; }
+        public DateTime? Created { get; set; }
+        public string? CoverURL { get; set; }
+
+        public Book()
+        {
+
+        }
+    }
+    
+
+}
